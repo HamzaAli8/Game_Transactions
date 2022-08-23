93a4bf489755fb396ab2e51217e8fc1de63ecf99
HamzaAli8 <hamzamalimaad@gmail.com>
2022-08-23 12:11:55 +0100

    update

--- src/main/java/com/hamza/gametransactionapi/models/Product.java
+++ src/main/java/com/hamza/gametransactionapi/models/Product.java
@@ -1,4 +1,25 @@
 package com.hamza.gametransactionapi.models;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table
 public class Product {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_Id")
+    private Long productId;
+
+    private String name;
+    private String description;
+
 }
